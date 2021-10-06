855d8daad513d37fa52817b1dd8f51f27db474fc
ehau <elie.hausmann@gmail.com>
2021-10-06 16:42:28 +0200

    initial input data added

--- passwordGenerator/passGen/forms.py
+++ passwordGenerator/passGen/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class PassForm(forms.Form):
-    length = forms.IntegerField(max_value = 94, min_value = 8, required = True)
+    length = forms.IntegerField(max_value = 94, min_value = 8, required = True, initial = 10)
