3cc10e901296ee6f26d9d1fad211254e780d9ebc
ehau <elie.hausmann@gmail.com>
2021-10-07 17:09:15 +0200

    alive time cookie changed

--- passwordGenerator/passwordGenerator/settings.py
+++ passwordGenerator/passwordGenerator/settings.py
@@ -54,7 +54,7 @@ SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
 SESSION_COOKIE_HTTPONLY = True
 
-SESSION_COOKIE_AGE = 600
+SESSION_COOKIE_AGE = 60
 
 ROOT_URLCONF = 'passwordGenerator.urls'
 
